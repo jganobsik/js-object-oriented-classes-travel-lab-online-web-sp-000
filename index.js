5bcb55a50729e7c570346d167bd65a5241a1da32
Joe Ganobsik <jganobsik@gmail.com>
2019-03-26 02:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+class Driver {
+  constructor( name, startDate ) {
+    this.name = name;
+    this.startDate = new Date( startDate );
+  }
+  yearsExperienceFromBeginningOf( year ) {
+    let endDate = new Date( year, 1, 1 );
+    let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 );
+    return parseInt( totalYears );
+  }
+}
 
+class Route {
+  constructor( beginningLocation, endingLocation ) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+blocksTravelled(){
+  eastWest.indexof(this.endingLocation = this.beginningLocation
+}  
+  
+  
+  
+  estimatedTime( peak ) {
+    if ( peak ) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
+
+
+
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
